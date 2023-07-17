04a2e07e95b4c3e302f0e52d3af95598e8ed30b6
Carlos Vivas <carsed@hotmail.com>
2023-07-17 02:10:03 -0400

    improve task form tailwind css

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -27,7 +27,7 @@ function TaskForm() {
 
   return (
     <div className="max-w-md mx-auto">
-      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4 rounded-md">
         <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
         <input
           placeholder="Escribe tu tarea"
@@ -42,7 +42,9 @@ function TaskForm() {
           value={description}
           className="bg-slate-300 p-3 w-full mb-2"
         ></textarea>
-        <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-400">Agregar</button>
+        <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-400 rounded-md">
+          Agregar
+        </button>
       </form>
     </div>
   );
